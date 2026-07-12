728feab95bc9215e528c7bdb21f12c8a6dc3c672
Vince Gabrielle Milos <gabriellodavinci@icloud.com>
2026-07-12 23:01:32 +0800

    allow all origins for CORS temporarily

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
